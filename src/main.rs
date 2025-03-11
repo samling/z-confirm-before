191e9026e3e8da83a04e0a75c1a3d4e9307f2ac7
Sam Boynton <samlingx@gmail.com>
2025-03-10 21:31:51 -0700

    Get a basic example working

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,20 @@ use zellij_tile::prelude::*;
 
 use std::collections::BTreeMap;
 
-#[derive(Default)]
 struct State {
-    // the state of the plugin
+    confirm_key: KeyWithModifier,
+    cancel_key: KeyWithModifier,
+    test_key: KeyWithModifier,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            confirm_key: KeyWithModifier::new(BareKey::Enter),
+            cancel_key: KeyWithModifier::new(BareKey::Esc),
+            test_key: KeyWithModifier::new(BareKey::Char('t')),
+        }
+    }
 }
 
 register_plugin!(State);
@@ -16,20 +27,36 @@ register_plugin!(State);
 
 impl ZellijPlugin for State {
     fn load(&mut self, configuration: BTreeMap<String, String>) {
-        // runs once on plugin load, provides the configuration with which this plugin was loaded
-        // (if any)
-        //
-        // this is a good place to `subscribe` (https://docs.rs/zellij-tile/latest/zellij_tile/shim/fn.subscribe.html)
-        // to `Event`s (https://docs.rs/zellij-tile/latest/zellij_tile/prelude/enum.Event.html)
-        // and `request_permissions` (https://docs.rs/zellij-tile/latest/zellij_tile/shim/fn.request_permission.html)
+        request_permission(&[PermissionType::ChangeApplicationState]);
+        request_permission(&[PermissionType::WriteToStdin]);
+        subscribe(&[EventType::Key]);
+
+        if let Some(confirm_key) = configuration.get("confirm_key") {
+            self.confirm_key = confirm_key.parse().unwrap_or(self.confirm_key.clone());
+        }
+        if let Some(abort_key) = configuration.get("cancel_key") {
+            self.cancel_key = abort_key.parse().unwrap_or(self.cancel_key.clone());
+        }
+        if let Some(test_key) = configuration.get("test_key") {
+            self.test_key = test_key.parse().unwrap_or(self.test_key.clone());
+        }
     }
     fn update(&mut self, event: Event) -> bool {
         let mut should_render = false;
-        // react to `Event`s that have been subscribed to (and the plugin has permissions for)
-        // return true if this plugin's `render` function should be called for the plugin to render
-        // itself
+        match event {
+            Event::Key(key) => {
+                if self.confirm_key == key {
+                    write("the plugin is working".as_bytes().to_vec());
+                    eprintln!("the plugin is working");
+                }
+            }
+            _ => {
+                eprintln!("some key registered");
+            },
+        };
         should_render
     }
+
     fn pipe (&mut self, pipe_message: PipeMessage) -> bool {
         let mut should_render = false;
         // react to data piped to this plugin from the CLI, a keybinding or another plugin
@@ -38,6 +65,7 @@ impl ZellijPlugin for State {
         // itself
         should_render
     }
+
     fn render(&mut self, rows: usize, cols: usize) {
         println!("Hi there! I have {rows} rows and {cols} columns");
     }
